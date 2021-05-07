2facf382bd0e999b10b1311aea1a7ac51ffe5a66
VeryHamburger <72227790+VeryHamburger@users.noreply.github.com>
2021-05-07 18:13:59 +0100

    Update index.js

--- index.js
+++ index.js
@@ -27,17 +27,18 @@ module.exports = {
    * @returns {AnimalObject | AnimalObject[]} The image and fact object.
    */
   async getAsync(type = 'random') {
-    const isArray = (Array.isArray(type) && type.length);
+    const isArray = Array.isArray(type);
     if (typeof type !== 'string' && !isArray && (type = type.flat()) && !type.every(t => typeof t === 'string')) throw new TypeError("'type' must be a string or an array of strings");
     
     type = type === 'random' ? animals[Math.floor(Math.random() * animals.length)] : !isArray ? type.toLowerCase() : [...new Set(type.map(t => t.toLowerCase()))];
     
     if (!isArray && !animals.includes(type)) throw new TypeError(`'${type}' is not a valid type, the valid types are: ${animals.join(', ')}, random`);
+ 
     if (isArray) return Promise.all(type.map(t => this.getAsync(t)));
 
     const [{ link: image }, { fact }] = await Promise.all([
       fetch(`${base}/img/${type}`).then(res => res.json()),
-      fetch(`${base}/fact/${type}`).then(res => res.json())
+      fetch(`${base}/facts/${type}`).then(res => res.json())
     ]).catch(err => {
       throw new Error(`Failed to get type '${type}' from API, error:\n${err}`);
     });
