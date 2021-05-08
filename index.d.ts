b9a613cf7c207f597d366c4164db8127a2ab9ee8
VeryHamburger <72227790+VeryHamburger@users.noreply.github.com>
2021-05-08 18:04:02 +0100

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,13 +1,13 @@
 declare module 'animality' {
-    interface AnimalObject {
-        name: string;
-        image: string;
-        fact: string;
-    }
+  interface AnimalObject {
+    name: string;
+    image: string;
+    fact: string;
+  }
 
-    namespace fns {
-        export function getAsync(type?: string | string[]): Promise<AnimalObject | AnimalObject[]>;
-    }
+  namespace fns {
+    export function getAsync(type?: string | string[]): Promise<AnimalObject | AnimalObject[]>;
+  }
 
-    export = fns;
+  export = fns;
 }
