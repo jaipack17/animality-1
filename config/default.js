9eec40c601101907f93688a1e96d77bf3e0aaa8a
jaipack17 <74130881+jaipack17@users.noreply.github.com>
2021-07-12 14:14:12 +0530

    initial

---
+++ config/default.js
@@ -0,0 +1,19 @@
+module.exports.animals = [
+  'cat',
+  'dog',
+  'bird',
+  'panda',
+  'redpanda',
+  'koala',
+  'fox',
+  'whale',
+  'kangaroo',
+  'bunny',
+  'lion',
+  'bear',
+  'frog',
+  'duck',
+  'penguin'
+];
+
+module.exports.base = 
